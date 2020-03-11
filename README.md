7d9f27190bb3bee10d6e76c3bc6f378dfacd8795
Leandro Andersen <leandro.andersen@adroll.com>
2020-03-11 00:20:17 +0000

    improvements on set, need further testing

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # easyfig
 A simple yet powerful configuration script automator based on python configparser
+
+This is a simple script to automate configuration persistence
+and accessibility throughout the system using python default config parser,
+the only thing you need to edit is the default_values field for the default
+settings desired, you may do this by inheriting this class or modifying
+that field before creating an instance.
+You can also provide a filename when instatiating if you desire so,
+instead of the standard config.ini. If the file does not exist,
+it will be created with the default settings.
+Multiple file names in a list are also possible, in which case all are used to load the
+settings in order, same settings take precedence on later files while
+only the most precedent last file is used for saving new settings
--- __init__.py
+++ __init__.py
@@ -1,18 +1,19 @@
 """ Author: Megarushing
-    This is a quick and dirty script to automate configuration persistence
+    This is a simple script to automate configuration persistence
     and accessibility throughout the system using python default config parser,
-    the only thing you need to edit is the _default_values field for the default
+    the only thing you need to edit is the default_values field for the default
     settings desired, you may do this by inheriting this class or modifying
-    the variable before creating an instance.
-    You can also provide a filename when instancing if you desire so,
+    that field before creating an instance.
+    You can also provide a filename when instatiating if you desire so,
     instead of the standard config.ini. If the file does not exist,
     it will be created with the default settings.
-    Multiple file names are also possible, in which case all are used to load the
-    settings in order, repeated settings take precedence on later files while
-    only the last file is used for saving new settings"""
+    Multiple file names in a list are also possible, in which case all are used to load the
+    settings in order, same settings take precedence on later files while
+    only the most precedent last file is used for saving new settings"""
 import os
 import sys
 import json
+import logging
 if sys.version_info >= (3, 0): #Python 3 configparser
     from configparser import ConfigParser
     from configparser import Error as ParserError
@@ -20,13 +21,16 @@ else:
     from ConfigParser import ConfigParser
     from ConfigParser import Error as ParserError
 
+logging.basicConfig()
+logger = logging.getLogger('easyfig')
+
 class Easyfig(object):
     # Here you should set all possible configs, and their default values
     # the exact config type will be inferred based on its default values type
     # every dict key will become a global variable under this lib for ease of access
     # NOTE: if config name starts with a _(underscore) it will be protected from the script
     # and only modifiable through manually editing the ini file
-    _default_values = {
+    default_values = {
         "GENERAL" : {
             "example" : 1,
             "_protected_example": 0
@@ -64,7 +68,7 @@ class Easyfig(object):
             self._parser.read(os.path.join(self._script_dir,filename))
 
         #generates all global variables representing config sections
-        for section,defaults in self._default_values.items():
+        for section,defaults in self.default_values.items():
             for option,default_value in defaults.items():
                 varname = option
                 if option.startswith("_"):
@@ -77,15 +81,15 @@ class Easyfig(object):
                 if section != "GENERAL":
                     varname = section.lower() + "_" + varname
                 #cast read string variable to type from default_values
-                val = self.get(option,section=section,default=default_value)
+                val = self.get(option,default=default_value,section=section)
                 self._set_attribute(varname, val, default_value)
         self.save()
         self.load_additional_sections()
 
     def _set_attribute(self, varname, value, default):
         """ Helper function to cast value type according to default value type
-         and setting the ambient global variable of variable_name"""
-        if type(default) in [list, dict]:  # load as json instead
+         from string and setting the ambient global variable of variable_name"""
+        if type(default) in [list, dict, tuple]:  # load as json instead
             try:
                 setattr(self,varname,json.loads(value.replace("'", '"')))
             except json.decoder.JSONDecodeError:
@@ -94,35 +98,41 @@ class Easyfig(object):
             try:
                 setattr(self,varname,type(default)(value))
             except Exception as e:
-                print("Error setting config variable",varname,", setting default value:",str(default),"\n",e)
+                logger.warning("Error setting config variable %s, keeping old value: %s", varname, e)
 
-    def get(self,key,section="GENERAL",default=None):
-        """gets config value from global config"""
+    def get(self,key,default=None,section="GENERAL"):
+        """gets config value string representation, if not possible
+         creates it with the default value provided"""
         value = default
         try:
             value = self._parser.get(section,key)
         except ParserError as e:
-            print("Error getting config",key,"from section",
-                  section,":",e,"\nSetting default value:",default)
-            if not self._save_parser.has_section(section):
-                self._save_parser.add_section(section)
-            if type(default) in [list, dict]:  # save as json instead
-                try:
-                    self._save_parser.set(section, key, json.dumps(value))
-                    default = json.dumps(value)
-                except json.decoder.JSONDecodeError:
-                    self._save_parser.set(section, key, str(value))
-            else:
-                self._save_parser.set(section, key, str(value))
-            value = default
+            logger.warning("Error getting config %s from section %s: %s\nSetting default value: %s",key,
+                  section,e,default)
+            value = self.set(key,value,section=section)
         return str(value)
 
     def set(self,option,value,section="GENERAL"):
+        """ Only allow setting values that previously exist in the defaults
+        also, do not allow setting protected values """
         if option in dict(self._parser.items(section)) and not option.startswith("_"):
-            self._save_parser.set(section,option,value)
+            if not self._save_parser.has_section(section):
+                self._save_parser.add_section(section)
+            #decide wether we should save it as small json or regular string
+            setval = value
+            if type(value) in [list, dict, tuple]:  # save as json instead
+                try:
+                    self._save_parser.set(section, option, json.dumps(value))
+                    setval = json.dumps(value)
+                except json.decoder.JSONDecodeError:
+                    self._save_parser.set(section, option, str(value))
+                    setval = str(value)
+            else:
+                self._save_parser.set(section, option, str(value))
+                setval = str(value)
             self.save()
             self._load()
-            return True
+            return setval
         else:
             return False
 
